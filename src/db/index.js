9b526f047f0782222346199eab3681e83cd0617a
vasu <vkkalavadiya21@gmail.com>
2025-05-12 15:15:28 +0530

    making setup and connections to mongoDB

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const DBConnection = async ()=>{
-    try {
-        const connect = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`)
-        console.log(`DB connected !! DB Host ${connect.connection.host}`);
-        }
-     catch (error) {
-        console.log(`Error Occurs in DBconnection ${error}`);
-        
-    }
-}
+const DBConnection = async () => {
+  try {
+    console.log(" Connecting to MongoDB...");
+    console.log(`Using URI: ${process.env.DB_URI}/${DB_NAME}`);
+    
+    const connect = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`);
+    
+    console.log(` DB connected! DB Host: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(` Error occurred in DB connection: ${error.message}`);
+  }
+};
 
-export default DBConnection
+export default DBConnection;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import dotenv from "dotenv"
-import DBConnection from "./db/index.js";
-
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+console.log(`index.js loaded`);
 dotenv.config({
-    path: './env'
-}
-)
-DBConnection()
+  path: './.env', // Correct path for .env file
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(" Error occurred while listening to the app:", error);
+      throw error;
+    });
+
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(` Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(` Error occurred in MongoDB connection: ${err}`);
+  });
